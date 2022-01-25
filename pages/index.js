f0ac785a5596f5c03cfdf6ddbba76ef19f41b981
amr shiha <amrshiha2@gmail.com>
2022-01-25 21:31:51 +0200

    add favicon

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,13 @@ export default function Home() {
       <Head>
         <title>Tour and Travel</title>
         <meta name="description" content="tour and travel app from figma to code using nextjs tailwindcss" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/images/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Mulish:wght@300;400;700&family=Playfair+Display:wght@700&display=swap" rel="stylesheet" />
